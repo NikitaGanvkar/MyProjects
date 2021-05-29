ed89d348fa08fc9594d3da7e86dc6ef6041b424b
NikitaGanvkar <nikita.ganvkar@gmail.com>
2021-05-29 15:15:09 +0530

    renamed

---
+++ WebDev/RestaurantUI/src/components/MainComponent.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+
+import { addComment, fetchDishes } from '../redux/ActionCreators';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) }
+});
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
+            );
+        }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail
+                    dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
+                />
+            );
+        }
+        return (
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+export default (withRouter(connect(mapStateToProps, mapDispatchToProps)(Main)));
+
+
---
+++ WebDev/RestaurantUI/src/shared/dishes.js
@@ -0,0 +1,43 @@
+export const DISHES =
+    [
+        {
+            id: 0,
+            name: 'Pizza',
+            image: '/assets/images/uthappizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            featured: true,
+            description: 'A decadent Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, chillies and Paneer tikka.'
+        },
+        {
+            id: 1,
+            name: 'Onion Rings',
+            image: '/assets/images/zucchipakoda.png',
+            category: 'appetizer',
+            label: '',
+            price: '1.99',
+            featured: false,
+            description: 'Deep fried rings of onions coated with parmesan cheese accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+            id: 2,
+            name: 'Garlic Bread',
+            image: '/assets/images/vadonut.png',
+            category: 'appetizer',
+            label: 'New',
+            price: '1.99',
+            featured: false,
+            description: 'Homemade bread, topped with garlic and olive oil, garnished with oregano'
+        },
+        {
+            id: 3,
+            name: 'Cheesecake',
+            image: '/assets/images/elaicheesecake.png',
+            category: 'dessert',
+            label: '',
+            price: '2.99',
+            featured: false,
+            description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with saffron'
+        }
+    ];
